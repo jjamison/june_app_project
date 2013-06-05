2f5be0460f208e0a3e95671e5d6d636a017f4c4b
Jay Jamison <jeremiah.s.jamison@gmail.com>
2013-06-05 13:35:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JuneAppProject::Application.config.secret_token = '8fde973e96666ca63c39c1a5dab3d08780efd8f70dd0b3ae8d9b7ffc09bc3bed8186b121c4f2c9592cee0c459469539ac938afb2117ebc337fcc571690a49be7'
